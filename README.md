4e49799f07ab4441c4a3ca99c832864fa48773ae
Jan Bouwhuis <jbouwh@hotmail.com>
2020-05-27 19:15:33 +0200

    Update code and documentation to a usable version

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # ha-entitiy-cache
 AppDaemon based entity cache application for Home Assistant
 
-This is application development yet.
+## Example config (/conf/appdaemon/apps/apps.yaml)
+```yaml
+ha-entity-cache:
+  module: entitycache
+  class: EntityCache
+# The cache_file setting is optional and defaults to cache.json in the script current folder
+  cache_file: /config/appdaemon/apps/ha-entity-cache/cache.json
+  entities:
+    input_select.test: option
+```
+
+Under `entities` place the entity names followed with the type of the entity that is to cached.
+Caching of attributes is _not_ supported with this version, but might be feature in future.
+Supported types are:
+* `option`: For caching input selects
+* `text`: For caching text inputs
+* `value`: For caching numeric inputs
+* `switch`: for caching on / off controls
+
+When an other type is supplied the state value will be set. This is not tested and might effect icons or other attributes if not set correctly.
+
+## Installation (With AppDaemon add on on Home Assistant)
+Unpack the script files at `/config/appdaemon/`. The active script is 
--- apps/ha-entity-cache/entitycache.py
+++ apps/ha-entity-cache/entitycache.py
@@ -17,7 +17,10 @@ class EntityCache(hass.Hass):
         if 'entities' in self.args:
             for entity in self.args['entities']:
                 self.handle[entity] = self.listen_state(self.callback, entity)
-                self.log(f"Cache enabled for entity: {entity}")            
+                self.log(f"Cache enabled for entity: {entity}")
+        else:
+            self.log("No entries found to cache", level='WARNING')
+            return
         try:
             with open(self.file, 'r') as self.filehandle:
                 self.cachedstate = json.load(self.filehandle)
@@ -26,10 +29,26 @@ class EntityCache(hass.Hass):
             # Recover the state from file
             for entity in self.cachedstate:
                 # Todo: replace set_state() with a better compatible: set_option(), set_text(), set_value() turn_on/off
-                self.set_state(entity, state=self.cachedstate[entity])
-                self.log(f"State recovered for entity {entity}: {self.cachedstate[entity]}")            
+                entity_type = self.args['entities'][entity]
+                if entity_type:
+                    if entity_type == 'option':
+                        self.select_option(entity, self.cachedstate[entity])
+                    elif entity_type == 'text':
+                        self.set_text(entity, self.cachedstate[entity])
+                    elif entity_type == 'value':
+                        self.set_value(entity, self.cachedstate[entity])
+                    elif entity_type == 'switch':
+                        if self.cachedstate[entity] == 'on':
+                            self.turn_on(entity)
+                        elif self.cachedstate[entity] == 'off':
+                            self.turn_on(entity)
+                    else:
+                        # Use the generic method if everyting else fails
+                        self.set_state(entity, state=self.cachedstate[entity])
+
+                self.log(f"State recovered for entity '{entity}': {self.cachedstate[entity]}")            
         except Exception as e:
-            self.log(f"Cache file '{self.file}' does not exist. No problem if this is the first time this application starts. Error: {e}", level='WARNING')
+            self.log(f"Cache file '{self.file}' could not be parsed. THis is no problem if this is the first time this application starts. Error: {e}", level='WARNING')
             raise e
 
 
@@ -39,15 +58,13 @@ class EntityCache(hass.Hass):
             self.log("Caching was stopped.")
 
     def callback(self, entity, attribute, old, new, kwargs):
-        self.log(f"Call back for {entity}:")
-        self.log(f"Old data: {old}")
-        self.log(f"New data: {new}")
         self.state[entity] = new
         # Update the state on file
         try:
             with open(self.file, 'w') as self.filehandleout:
                 json.dump(self.state, self.filehandleout)
             self.filehandleout.close()
+            self.log(f"Cache file updated '{entity}' = '{new}'", level='INFO')
         except Exception as e:
             self.log(f"Cache file '{self.file}' was not updated. Check your configuration.", level='ERROR')
 
